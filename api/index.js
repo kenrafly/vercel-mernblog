298d991e01f1a16ab460e6366994e7f0620868f0
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2024-06-13 23:47:19 +0700

    frontenndelete1

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 dotenv.config();
 
@@ -20,7 +19,6 @@ mongoose
   });
 
 const app = express();
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -37,12 +35,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
